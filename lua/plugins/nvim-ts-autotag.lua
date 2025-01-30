b219f9b8471b50ad6b5c14a235796023729159b6
WaffleMonster <rachkov.daniil2001@gmail.com>
2025-01-30 13:47:05 +0300

    mason

--- lua/plugins/nvim-ts-autotag.lua
+++ lua/plugins/nvim-ts-autotag.lua
@@ -2,9 +2,9 @@ return {
   "nvim-ts-autotag",
   opts = function(_, opts)
     -- Defaults
-    opts.enable_close = true
-    opts.enable_rename = true -- Auto rename pairs of tags
-    opts.enable_close_on_slash = false -- Auto close on trailing </
+    -- opts.enable_close = true
+    -- opts.enable_rename = true -- Auto rename pairs of tags
+    -- opts.enable_close_on_slash = false -- Auto close on trailing </
     -- Also override individual filetype configs, these take priority.
     -- Empty by default, useful if one of the "opts" global settings
     -- doesn't work well in a specific filetype
